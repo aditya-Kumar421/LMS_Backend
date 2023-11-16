64a5cb66bef33d99c27d0e1fbf82194a0801ba26
Aditya <ak.adityakumar421@gmail.com>
2023-11-16 21:35:43 +0530

    auth without Api

--- Authentication_/authentication/mainapp/forms.py
+++ Authentication_/authentication/mainapp/forms.py
@@ -4,3 +4,18 @@ from django.contrib.auth.models import User
 class LoginForm(forms.Form):
     Username = forms.CharField()   #take a look on ()section
     password = forms.CharField(widget=forms.PasswordInput)
+
+#signup:
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields =('username', 'first_name', 'last_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data      #------tal-------
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Password don\'t match.')
+        return cd['password2']
--- Authentication_/authentication/mainapp/urls.py
+++ Authentication_/authentication/mainapp/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
+    #registration:
+    path('register/', views.register, name='register'),
 ]
