d16b012d369b88a702d2c6975a934d4b58f9c03a
Aditya Kumar <133591442+aditya-Kumar421@users.noreply.github.com>
2023-11-21 19:00:48 +0530

    Delete Authentication_ directory

--- Authentication_/mainapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms 
-from django.contrib.auth.models import User
-
-class LoginForm(forms.Form):
-    email = forms.EmailField()   #take a look on ()section
-    password = forms.CharField(widget=forms.PasswordInput)
